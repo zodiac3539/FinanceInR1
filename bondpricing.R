5226372a052a83f20057338ee696c15c6619a751
zodiac3539 <zodiac3539@naver.com>
2016-02-24 17:31:12 -0500

    Add bond pricing
    Add bond pricing function.

---
+++ bondpricing.R
@@ -0,0 +1,19 @@
+f.bondprice = function (mature, coupon, par, discount)
+{
+  E = NULL
+  sum_e = 0;
+  if(mature == 1) {
+    E[1] <- (coupon +par) /(1+discount)
+    sum_e <- sum(E)
+  } else {
+    for (i in 1:mature) 
+    { 
+        E[i] <- coupon/((1+discount)^i)
+    } 
+    E[mature] <- E[mature] + par/((1+discount)^mature)
+    sum_e <- sum(E)
+  }
+
+  return (sum_e)
+}
+
